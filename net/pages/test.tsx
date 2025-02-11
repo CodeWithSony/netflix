cb8dc15bdb2b77a64f187a177ed78511d3c8b8d8
CodeWithSony <priyanka.sahu@techub.in>
2025-02-11 15:48:59 +0530

    removed

--- net/pages/test.tsx
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export default function Home() {
-  return <h1>I m component</h1>;
-  <div></div>;
-}
--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-// 12: 8  image is not coming
-
-export default function Home() {
-  return <h1>Netflix Clone</h1>;
-}
